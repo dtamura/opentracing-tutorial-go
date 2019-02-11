5921a2b0a5700f0be0c18ea52a9cb669d1de6485
Daiki Tamura <dtamura23@outlook.jp>
2019-02-11 14:15:38 +0900

    formatサービス追加

---
+++ cmd/formatter.go
@@ -0,0 +1,62 @@
+// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/dtamura/opentracing-tutorial-go/lib/log"
+	"github.com/dtamura/opentracing-tutorial-go/lib/tracing"
+	"github.com/dtamura/opentracing-tutorial-go/services/formatter"
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+var formatterOptions formatter.ConfigOptions
+
+// formatterCmd represents the formatter command
+var formatterCmd = &cobra.Command{
+	Use:   "formatter",
+	Short: "Start formatter Service",
+	Long:  "Start formatter service",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		zapLogger := logger.With(zap.String("service", "lesson03"))
+		logger := log.NewFactory(zapLogger)
+		tracer, closer := tracing.Init("formatter", logger) // lesson03というサービス名のtracerを生成
+		defer closer.Close()
+		opentracing.SetGlobalTracer(tracer) // to start the new spans, so we need to initialize that global variable to our instance of Jaeger tracer
+
+		formatterOptions.Port = 8081
+		formatter := formatter.NewServer(
+			formatterOptions,
+			tracer,
+			logger,
+		)
+		return formatter.RunE()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(formatterCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// formatterCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// formatterCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ services/formatter/main.go
@@ -0,0 +1,52 @@
+package formatter
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"strconv"
+
+	"github.com/dtamura/opentracing-tutorial-go/lib/log"
+	opentracing "github.com/opentracing/opentracing-go"
+	"go.uber.org/zap"
+)
+
+// Server フォーマットサービス
+type Server struct {
+	addr   string
+	tracer opentracing.Tracer
+	logger log.Factory
+}
+
+// ConfigOptions オプション
+type ConfigOptions struct {
+	Port int // Listenするポート番号
+}
+
+// NewServer formatter.Server を作成
+func NewServer(options ConfigOptions, tracer opentracing.Tracer, logger log.Factory) *Server {
+	return &Server{
+		addr:   net.JoinHostPort("0.0.0.0", strconv.Itoa(options.Port)),
+		tracer: tracer,
+		logger: logger,
+	}
+}
+
+// RunE サーバーを開始する
+func (s *Server) RunE() error {
+	mux := s.createServerMux()
+	s.logger.Bg().Info("Starting Formatter Server", zap.String("address", s.addr))
+	return http.ListenAndServe(s.addr, mux)
+}
+
+func (s *Server) createServerMux() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/format", s.formatString)
+	return mux
+}
+
+func (s *Server) formatString(w http.ResponseWriter, r *http.Request) {
+	helloTo := r.FormValue("helloTo")
+	helloStr := fmt.Sprintf("Hello, %s!", helloTo)
+	w.Write([]byte(helloStr))
+}
