5266a6f08f673ed8d79b8368b56b3fafce73d197
Daiki Tamura <dtamura23@outlook.jp>
2019-02-11 12:52:00 +0900

    lesson02追加

---
+++ cmd/lesson02.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"github.com/dtamura/opentracing-tutorial-go/lib/log"
+	"github.com/dtamura/opentracing-tutorial-go/lib/tracing"
+	"github.com/dtamura/opentracing-tutorial-go/services/lesson02"
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+var lesson02Options = &lesson02.ConfigOptions{}
+
+// lesson02Cmd represents the lesson02 command
+var lesson02Cmd = &cobra.Command{
+	Use:   "lesson02",
+	Short: "start lesson02 program",
+	Long:  "Start lesson02 Program",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		zapLogger := logger.With(zap.String("service", "lesson02"))
+		logger := log.NewFactory(zapLogger)
+		tracer, closer := tracing.Init("lesson02", logger) // lesson02というサービス名のtracerを生成
+		opentracing.SetGlobalTracer(tracer)                // to start the new spans, so we need to initialize that global variable to our instance of Jaeger tracer
+		client := lesson02.NewClient(
+			lesson02Options,
+			tracer,
+			logger,
+			closer,
+		)
+		defer closer.Close()
+		err := client.RunE()
+		return err
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(lesson02Cmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// lesson02Cmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// lesson02Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	lesson02Cmd.PersistentFlags().StringVarP(&lesson02Options.Message, "message", "m", "tamura", "Message") // -mオプションで文字列を取得する
+
+}
---
+++ services/lesson02/main.go
@@ -0,0 +1,77 @@
+package lesson02
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/dtamura/opentracing-tutorial-go/lib/log"
+	opentracing "github.com/opentracing/opentracing-go"
+	spanLog "github.com/opentracing/opentracing-go/log"
+)
+
+// Client 構造体
+type Client struct {
+	tracer opentracing.Tracer
+	logger log.Factory
+	closer io.Closer
+}
+
+// ConfigOptions オプション
+type ConfigOptions struct {
+	Message string
+}
+
+var options = &ConfigOptions{}
+
+// NewClient Client構造体を作成する
+func NewClient(o *ConfigOptions, tracer opentracing.Tracer, logger log.Factory, closer io.Closer) *Client {
+	options = o
+	return &Client{
+		tracer: tracer,
+		logger: logger,
+		closer: closer,
+	}
+}
+
+// RunE プログラム開始。エラーを返す
+func (c *Client) RunE() error {
+	c.logger.Bg().Info("Lesson02 Start")
+
+	span := c.tracer.StartSpan("say-hello") // "say-hello" という名称のSpanを生成
+	ctx := context.Background()
+	ctx = opentracing.ContextWithSpan(ctx, span)
+
+	helloStr := options.Message
+	span.SetTag("hello-to", helloStr) // Tagに"hello-to"をセット
+	defer span.Finish()
+
+	str := formatString(ctx, helloStr)
+	printHello(ctx, str)
+
+	return nil
+}
+
+func formatString(ctx context.Context, helloTo string) string {
+	// span := rootSpan.Tracer().StartSpan("formatString")
+	span, _ := opentracing.StartSpanFromContext(ctx, "formatString")
+	defer span.Finish()
+
+	helloStr := fmt.Sprintf("Hello, %s!", helloTo)
+	span.LogFields(
+		spanLog.String("event", "string-format"),
+		spanLog.String("value", helloStr),
+	)
+
+	return helloStr
+}
+
+// printHello は文字列を出力する
+func printHello(ctx context.Context, helloStr string) {
+	// span := rootSpan.Tracer().StartSpan("printHello")
+	span, _ := opentracing.StartSpanFromContext(ctx, "printHello")
+	defer span.Finish()
+
+	println(helloStr)
+	span.LogKV("event", "println")
+}
